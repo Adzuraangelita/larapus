9244cbc5ef6392607344a11897266ef535537dfc
adzuraangelita <adzuraangel13@gmail.com>
2021-11-13 11:21:43 +0700

    book

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Author;
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $author = Author::all();
+        return view('author.index', compact('author'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('author.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // validasi data
+        $validated = $request->validate([
+            'name'=>'required',
+        ]);
+
+        $author = new Author;
+        $author-> name = $request->name;
+        $author->save();
+        return redirect()->route('author.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $author = Author::findOrFail($id);
+        return view('author.show', compact('author'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $author = Author::findOrFail($id);
+        return view('author.edit', compact('author'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //validasi data
+        $validated = $request->validate([
+            'name' => 'required',
+        ]);
+
+        $author = Author::findOrFail($id);
+        $author->name = $request->name;
+        $author->save();
+        return redirect()->route('author.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $author= Author::findOrFail($id);
+        $author->delete();
+        return redirect()->route('author.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,20 +14,45 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    Route::group(['prefix'=>'admin', 'middleware'=>['auth','role:admin']], function () {
-        Route::get('/',function () {
-            return view('admin');
-        });
+Route::group(
+    ['prefix'=>'admin','middleware'=>['auth','role:admin']], function(){
+        Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     });
+
+    Route::group(
+        ['prefix'=>'user','middleware'=>['auth','role:member']], function(){
+            Route::get('/sample', [App\Http\Controllers\UserController::class, 'index2'])->name('home');
+        });
+
+        Route::resource('author', AuthorController::class);
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Auth::routes();
+
+//     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//     Route::group(['prefix'=>'admin', 'middleware'=>['auth','role:admin']], function () {
+//         Route::get('/',function () {
+//             return view('admin');
+//         });
+//     });
      // Route diisi disini...
 // Route::get('tes-admin', function() {
 //     return view('layouts.admin');
 // });
+Auth::routes();
+
+Route::group(
+    ['prefix'=>'admin','middleware'=>['auth','role:admin']], function(){
+        Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    });
+
+    Route::group(
+        ['prefix'=>'user','middleware'=>['auth','role:member']], function(){
+            Route::get('/sample', [App\Http\Controllers\UserController::class, 'index2'])->name('home');
+        });
+        Route::resource('author', AuthorController::class);
+        Route::resource('book', BookController::class);
